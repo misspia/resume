fecd4e9acc7da7ebb8b3a63fb64ac1a1b21218ca
andrewjungg <andrewjung.euihyun@gmail.com>
2016-08-30 23:55:32 -0400

    error fix

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './components/App';
-import HomePage from './components/HomePage';
 import MainPage from './containers/MainPage'; // eslint-disable-line import/no-named-as-default
 import Menu from './components/Menu.js';
 import NotFoundPage from './components/NotFoundPage.js';
