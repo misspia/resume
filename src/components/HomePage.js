ae3563bbf2f60231028e0924741677bf42f84a25
andrewjungg <andrewjung.euihyun@gmail.com>
2016-09-02 19:08:55 -0400

    delete useless code

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,9 +1,5 @@
 import React from 'react';
-import {Link} from 'react-router';
-import $ from 'jquery';
-import axios from 'axios';
 import '../styles/HomePage.css';
-import PokemonProfile from './PokemonProfile';
 
 // Since this component is simple and static, there's no parent container for it.
 
