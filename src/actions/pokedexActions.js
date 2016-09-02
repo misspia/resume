eee607a5c0b70d959accc6922f62eb2963371b77
andrewjungg <andrewjung.euihyun@gmail.com>
2016-09-02 19:51:58 -0400

    params

--- src/actions/pokedexActions.js
+++ src/actions/pokedexActions.js
@@ -14,18 +14,18 @@ export function getPokemonSuccess(settings) {
   };
 }
 
-export function getPokemon(settings) {
+export function getPokemon(id) {
   return function (dispatch) {
     // thunks allow for pre-processing actions, calling apis, and dispatching multiple actions
     // in this case at this point we could call a service that would persist the fuel savings
 
-    return dispatch(service.getPokeData()).then(() => {
-      return getPokemonSuccess();
+    return dispatch(service.getPokeData(id)).then((response) => {
+      return getPokemonSuccess(response);
     });
   };
 }
 
-export function getSpecies(settings, fieldName, value) {
+export function getSpecies(id)) {
   return {
     type: types.GET_SPECIES,
     dateModified: dateHelper.getFormattedDateTime(),
