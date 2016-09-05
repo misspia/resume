ce5eed43711ddc37304bb4b0b33b601837c4f6c8
andrewjungg <andrewjung.euihyun@gmail.com>
2016-09-04 21:58:25 -0400

    send down data as proptypes

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -7,8 +7,6 @@ import PokemonProfile from './PokemonProfile';
 
 // Since this component is simple and static, there's no parent container for it.
 
-var pokeId = 30;
-var baseUrl = 'http://pokeapi.co/api';
 const Menu = React.createClass ({
 	componentDidMount() {
 	},
@@ -20,73 +18,64 @@ const Menu = React.createClass ({
 		};
 	},
 
-	getPokemon() {
-			var getUrl = baseUrl +'/v2/pokemon/' + pokeId;
-			$.ajax({
-		      url: getUrl,
-		      dataType: 'json',
-		      cache: false,
-		      success: function(data) {
-		        this.setState({
-              pokemon: data
-            });
-		      }.bind(this),
-		      error: function(xhr, status, err) {
-		        console.error(getUrl, status, err.toString());
-		      }.bind(this)
-    		});
-			
-			console.log(this.state.data);
-			var pokeGeneral = this.state.data;
-			var pokeStat = this.state.data.stats;
-			var pokeType = this.state.data.types;
-			console.log(pokeGeneral.name);
-			console.log(pokeGeneral.id);
-			console.log(pokeStat);
-			console.log(pokeType);
+	getPokemon(pokemon) {
+    const baseUrl = 'http://pokeapi.co/api';
+		const getUrl = baseUrl +'/v2/pokemon/' + pokemon;
+		$.ajax({
+      url: getUrl,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({
+          pokemon: data
+        });
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(getUrl, status, err.toString());
+      }.bind(this)
+		});
 	},
 
-	getSpecies() {
-			var getUrl = baseUrl +'/v2/pokemon-species/' + pokeId;
-			$.ajax({
-		      url: getUrl,
-		      dataType: 'json',
-		      cache: false,
-		      success: function(data) {
-		        this.setState({
-              species: data
-            });
-		      }.bind(this),
-		      error: function(xhr, status, err) {
-		        console.error(getUrl, status, err.toString());
-		      }.bind(this)
-    		});
-		        var pokeSpecies = this.state.data.genera[0].genus;
-		        var pokeDesc = this.state.data.flavor_text_entries[1].flavor_text;
-	    		console.log(pokeSpecies);
-	    		console.log(pokeDesc);
+	getSpecies(pokemon) {
+    const baseUrl = 'http://pokeapi.co/api';
+		const getUrl = baseUrl +'/v2/pokemon-species/' + pokemon;
+		$.ajax({
+      url: getUrl,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({
+          species: data
+        });
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(getUrl, status, err.toString());
+      }.bind(this)
+		});
 	},
 
   getData() {
-    const pokemonName = ReactDOM.findDOMNode('search').value;
-    this.getPokemon();
-    this.getSpecies();
+    const pokemon = ReactDOM.findDOMNode('search').value;
+    this.getPokemon().bind(this, pokemon);
+    this.getSpecies().bind(this, pokemon);
   },
 
 	render () {
-	return (
-
-			<div className="container">
-				<div className ="row">
-					<div className="col s4">
-						<input ref='search' type='text' placeholder='Whos that Pokemon?' />
-						<button onClick={this.getData}>Click</button>
-					</div>
-					<PokemonProfile />
-				</div>
-			</div>
-	)}
-});
+  	return (
+  		<div className="container">
+  			<div className ="row">
+  				<div className="col s4">
+  					<input ref='search' type='text' placeholder='Whos that Pokemon?' />
+  					<button onClick={this.getData}>Click</button>
+  				</div>
+  				<PokemonProfile
+            pokemonData={this.state.pokemon}
+            speciesData={this.state.species}
+          />
+  			</div>
+  		</div>
+  	)}
+  });
 
 export default Menu;
 
--- src/components/PokemonProfile.js
+++ src/components/PokemonProfile.js
@@ -1,26 +1,32 @@
 import React, {PropTypes} from 'react';
 
-const PokemonProfile = (props) => {
-  const handleChange = (e) => {
-    props.onChange(props.name, e.target.value);
-  };
-  const pokeName = 'lugia';
-  const getImage = "http://www.pokestadium.com/sprites/xy/" + pokeName + ".gif";
-  return (
-    <div className='col s8'>
-      <div className='row'>
-        <div className ='col s4'>
-          <img src={getImage}/>
-        </div>
-        <div className='cols s8'>
-          
+const PokemonProfile = React.createClass ({
+  propTypes: {
+    pokemon: React.PropTypes.object,
+    species: React.PropTypes.object
+  },
+
+  render() {
+  const pokemon = this.props.pokemon;
+  const pokeStat = pokemon.stats;
+  const pokeType = pokemon.types;
+  const species = this.props.species;
+  const pokeSpecies = species.genera[0].genus;
+  const pokeDesc = species.flavor_text_entries[1].flavor_text;
+  const getImage = "http://www.pokestadium.com/sprites/xy/" + pokemon + ".gif";
+    return (
+      <div className='col s8'>
+        <div className='row'>
+          <div className ='col s4'>
+            <img src={getImage}/>
+          </div>
+          <div className='cols s8'>        
+          </div>
         </div>
       </div>
-      
-
-    </div>
-  );
-};
+    );
+  }
+});
 
 
 
